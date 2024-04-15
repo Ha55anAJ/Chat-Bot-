c1f2c41395a04d5ba9db3f02436765fa9e3ce2d8
Ha55anAJ <144936208+Ha55anAJ@users.noreply.github.com>
2024-04-15 16:58:16 +0500

    Delete OOPSPROJ directory

--- OOPSPROJ/openai-cpp/examples/00-showcase.cpp
+++
@@ -1,9 +0,0 @@
-#include "openai.hpp"
-
-#include <iostream>
-
-int main() {
-    openai::start(); // Will use the api key provided by `OPENAI_API_KEY` environment variable
-    // openai::start("your_API_key", "optional_organization"); // Or you can handle it yourself
-
-}
